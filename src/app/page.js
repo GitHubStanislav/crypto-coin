cd91545a9a1b2f6bd9e81b5908cf5a099b5bfe3e
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-06-03 18:22:23 +0300

    style: add background gradient to body

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 
 export default function Home() {
   return (
-    <></>
+      <div className="bg-gradient-to-b from-purple-950 to-pink-950 h-screen">
+        YYYYYYYYYYYYYY
+      </div>
   )
 }
