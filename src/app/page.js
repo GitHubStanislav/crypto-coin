1e03b1358b3e4d03871425890bc025f59cae04cb
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-06-03 18:38:11 +0300

    add nav

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,30 @@
 export default function Home() {
   return (
       <div className="bg-gradient-to-b from-purple-950 to-pink-950 h-screen">
-        YYYYYYYYYYYYYY
+        <nav>
+            <div className='flex'>
+                <a href="#" className="text-4xl font-bold text-white">
+                    CRYPTO-COIN
+                </a>
+                <ul className="flex space-x-4">
+                    <li>
+                        <a href="#" className=" font-bold text-white">
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="font-bold text-white">
+                            Market
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" className="font-bold text-white">
+                            Join us
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
       </div>
   )
 }
