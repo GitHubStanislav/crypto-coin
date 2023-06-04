ea776db0902116a2bd22a0e8fe83f6ffa443575c
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-06-04 12:05:27 +0300

    style: add margin in modal

--- src/app/Navbar/MenuItems.js
+++ src/app/Navbar/MenuItems.js
@@ -6,14 +6,14 @@ const MenuItems = ({ isMenuOpen }) => {
     <ul
       className={`${
         isMenuOpen ? "block" : "hidden"
-      } md:flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 mt-4 md:mt-0`}
+      } md:flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 mt-4 ml-4 md:mt-0`}
     >
       <li>
         <a
           href="#"
           className={`font-bold ${
             isMenuOpen ? "text-black" : "text-white"
-          } transition-colors duration-300 hover:text-pink-500 block`}
+          } transition-colors duration-300 hover:text-pink-500 block text-xl`}
         >
           Home
         </a>
@@ -23,7 +23,7 @@ const MenuItems = ({ isMenuOpen }) => {
           href="#"
           className={`font-bold ${
             isMenuOpen ? "text-black" : "text-white"
-          } transition-colors duration-300 hover:text-pink-500 block`}
+          } transition-colors duration-300 hover:text-pink-500 block text-xl`}
         >
           Market
         </a>
@@ -33,7 +33,7 @@ const MenuItems = ({ isMenuOpen }) => {
           href="#"
           className={`font-bold ${
             isMenuOpen ? "text-black" : "text-white"
-          } transition-colors duration-300 hover:text-pink-500 block`}
+          } transition-colors duration-300 hover:text-pink-500 block text-xl`}
         >
           Join us
         </a>
--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ const Home = () => {
   };
 
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-12">
       <Navbar
         isMenuOpen={isMenuOpen}
         handleMenuToggle={handleMenuToggle}
