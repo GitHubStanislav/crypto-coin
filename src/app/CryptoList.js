a22d1658390c6735bbe83b5abfd97149a90a0bbb
Stas Spiridonov <spiridonov.kyiv@gmail.com>
2023-06-04 20:19:55 +0300

    +

--- src/app/CryptoList.js
+++ src/app/CryptoList.js
@@ -20,7 +20,7 @@ const CryptoList = ({ cryptocurrencies, exchangeRate }) => {
 
   return (
     <ul className="space-y-4 rounded-md divide-y divide-pink-800">
-      <p className="mb-32 text-center ml-10 text-6xl bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text">
+      <p className="mb-36 text-center ml-10 text-6xl bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text">
         ALL CRYPTOCURRENCIES
       </p>
       <div className="bg-gradient-to-r from-indigo-500 to-pink-500 text-transparent bg-clip-text text-white flex justify-end mr-8">
